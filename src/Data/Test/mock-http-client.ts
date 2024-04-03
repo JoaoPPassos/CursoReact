948aa033b8a2f004b60162e6b42e744f6e22971e
joao_passos <joao.passos@teamsoft.com.br>
2024-04-03 20:27:17 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/Data/Test/mock-http-client.ts
+++ src/Data/Test/mock-http-client.ts
@@ -2,13 +2,18 @@ import {
   HttpPostClient,
   HttpPostParams,
 } from "@/Data/Protocols/Http/http-post-client";
+import { HttpResponse, HttpStatusCode } from "../Protocols/Http/http-response";
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
   body?: object;
-  async post(params: HttpPostParams) {
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.OK,
+  };
+
+  async post(params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url;
     this.body = params.body;
-    return Promise.resolve();
+    return Promise.resolve(this.response);
   }
 }
--- src/Data/UseCases/Authentication/remote-authentication.spec.ts
+++ src/Data/UseCases/Authentication/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import { mockAuthentication } from "@/Domain/Test/mock-authentication";
 import { HttpPostClientSpy } from "@/Data/Test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
 import { faker } from "@faker-js/faker";
+import { InvalidCredentialError } from "@/Domain/Errors/invalid-credentials-error";
+import { HttpStatusCode } from "@/Data/Protocols/Http/http-response";
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -28,5 +30,16 @@ describe("RemoteAuthentication", () => {
     await sut.auth(authentication);
 
     expect(httpPostClientSpy.body).toEqual(authentication);
+  });
+
+  test("Should throw InvalidCredentialError if HttpPostClient returns 401", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.UNAUTHORIZED,
+    };
+    const authentication = mockAuthentication();
+    const promise = sut.auth(authentication);
+
+    await expect(promise).rejects.toThrow(new InvalidCredentialError());
   });
 });
