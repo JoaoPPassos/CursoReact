628beab03e17fb937f141be378a7c87ba56f1cb0
joao_passos <joao.passos@teamsoft.com.br>
2024-04-04 17:31:49 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/Infra/Http/AxiosHttpClient/axios-http-client.ts
+++ src/Infra/Http/AxiosHttpClient/axios-http-client.ts
@@ -1,8 +1,16 @@
-import { HttpPostParams, HttpResponse } from "@/Data/Protocols/Http";
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse,
+} from "@/Data/Protocols/Http";
 import axios from "axios";
 
-export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body);
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const response = await axios.post(params.url, params.body);
+    return {
+      statusCode: response.status,
+      body: response.data,
+    };
   }
 }
