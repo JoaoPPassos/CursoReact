2888d57ad9a5c17bd521d2310f937acbe10a8182
joao_passos <joao.passos@teamsoft.com.br>
2024-04-04 17:20:15 -0300

    test: ensure AxiosHttpClient calls axios with correct body

--- src/Infra/Http/AxiosHttpClient/axios-http-client.spec.ts
+++ src/Infra/Http/AxiosHttpClient/axios-http-client.spec.ts
@@ -18,19 +18,11 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 });
 
 describe("AxiosHttpClient", () => {
-  test("Should call axios with correct URL and verb", async () => {
-    const url = faker.internet.url();
-    const sut = makeSut();
-    await sut.post({ url });
-
-    expect(mockedAxios.post).toHaveBeenCalledWith(url);
-  });
-
-  test("Should call axios with correct body", async () => {
+  test("Should call axios with correct values", async () => {
     const request = mockPostRequest();
     const sut = makeSut();
     await sut.post(request);
 
-    expect(mockedAxios).toHaveBeenCalledWith(request.url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 });
